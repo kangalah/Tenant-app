1a96c6a2fc93445841ed293025480708e71c2977
MUTHAKABRIAN <muthakabrian@gmail.com>
2020-05-22 03:25:24 +0300

    add function for collecting input data from contact us form

--- js/script.js
+++ js/script.js
@@ -12,18 +12,18 @@ $(document).ready(function () {
   $(".file-upload").on("change", function () {
     readURL(this);
   });
-  function Profile(fName,lName,phone,email,houseNumber,password){
-      this.fName=fName;
-      this.lName=lName;
-      this.phone=phone;
-      this.email=email;
-      this.houseNumber=houseNumber;
-      this.password=password;
+  function Profile(fName, lName, phone, email, houseNumber, password) {
+    this.fName = fName;
+    this.lName = lName;
+    this.phone = phone;
+    this.email = email;
+    this.houseNumber = houseNumber;
+    this.password = password;
   }
 
   $("#submit").click(function (event) {
     event.preventDefault();
-    var image=$("input#inputFileToLoad").val();
+    var image = $("input#inputFileToLoad").val();
     var fName = $("input#first_name").val();
     var lName = $("input#last_name").val();
     var phone = $("input#phone").val();
@@ -34,22 +34,40 @@ $(document).ready(function () {
     if (pword !== pword2) {
       alert("unmatched password");
     }
-    
-    var tenantNames =[];
+
+    var tenantNames = [];
     // $("#profile").append("<img src="+"image" +"width='564px'"+"height='376px'"+"alt=''>");
-    var profile = new Profile(fName,lName,phone,email,house,password);
+    var profile = new Profile(fName, lName, phone, email, house, password);
     tenantNames.push(profile);
     console.log(tenantNames);
 
-    $("#tenant1").text(tenantNames[0].fName+tenantNames[0].lName);
+    $("#tenant1").text(tenantNames[0].fName + tenantNames[0].lName);
     $("#tenantFName1").text(tenantNames[0].fName);
     $("#tenantLName1").text(tenantNames[0].lName);
     $("#tenantPhoneNumber1").text(tenantNames[0].phone);
     $("#tenantHouseNumber1").text(tenantNames[0].houseNumber);
     $("#tenantEmail1").text(tenantNames[0].email);
- 
+  });
+
+  $("#send").click(function (event) {
+    event.preventDefault();
+    var name = $("#defaultContactFormName").val();
+    var email = $("#defaultContactFormEmail").val();
+    var message = $("#exampleFormControlTextarea2").val();
+    var feedback = $("#feedback").val();
+    alert(
+      "Thank you " + name + ". For your Concerns, We'll get back to you soon!"
+    );
+  });
+  $("#login").click(function (event) {
+    event.preventDefault();
+    var email = $("#email1").val();
+    var password = $("#password1").val();
+    if (email !== null && password !== null) {
+        window.location.href = "profile.html";
+    } else {
+     
+      alert("enter your email and password");
+    }
   });
 });
-
-
-
